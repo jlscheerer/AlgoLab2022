bbda1e01d44291fd639a2bc8afe64f75112a0f1f
Jan Luca Scheerer <scheerer@cs.tum.edu>
2023-01-12 15:21:40 +0100

    Update H1N1 & Knights

--- src/week08/H1N1.cc
+++ src/week08/H1N1.cc
@@ -2,68 +2,51 @@
 
 #include <CGAL/Delaunay_triangulation_2.h>
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-
-using Kernel = CGAL::Exact_predicates_inexact_constructions_kernel;
-using Triangulation = CGAL::Delaunay_triangulation_2<Kernel>;
-using Edge_iterator = Triangulation::Edge_iterator;
-using Face_iterator = Triangulation::Finite_faces_iterator;
+#include <CGAL/Triangulation_face_base_with_info_2.h>
+#include <CGAL/Triangulation_vertex_base_2.h>
+#include <boost/pending/disjoint_sets.hpp>
 
 using namespace std;
 
-using Point = Kernel::Point_2;
+using K = CGAL::Exact_predicates_inexact_constructions_kernel;
+using Vb = CGAL::Triangulation_vertex_base_2<K>;
+using Fb = CGAL::Triangulation_face_base_with_info_2<int, K>;
+using Tds = CGAL::Triangulation_data_structure_2<Vb, Fb>;
+using Delaunay = CGAL::Delaunay_triangulation_2<K, Tds>;
+
+using Point = K::Point_2;
+
+constexpr int INFINITE_FACE = 0;
 
 struct Edge {
   int neighbor;
   long distance;
 };
 
-int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
-  while (true) {
-    int n, m, x, y;
-    long d;
-    cin >> n;
-    if (!n)
-      break;
-    vector<Point> points;
-    points.reserve(n);
-    for (int i = 0; i < n; ++i) {
-      cin >> x >> y;
-      points.emplace_back(x, y);
+struct Area {
+  Area(const int n, vector<Point> &points) : t(points.begin(), points.end()) {
+    int curr_id = 1;
+    for (auto f = t.all_faces_begin(); f != t.all_faces_end(); ++f) {
+      f->info() = t.is_infinite(f) ? INFINITE_FACE : curr_id++;
     }
-    Triangulation t(points.begin(), points.end());
-    const int INFINITE_FACE = 0;
-    unordered_map<void *, int> ids;
-    ids[nullptr] = INFINITE_FACE; // reserve "INFINITE_FACE"
-    auto id_face = [&](auto f) {
-      if (t.is_infinite(f))
-        return INFINITE_FACE;
-      void *addr = &*f;
-      if (ids.find(addr) == ids.end()) {
-        ids[addr] = ids.size();
-      }
-      return ids[addr];
-    };
 
     unordered_map<int, vector<Edge>> adj;
-    for (Face_iterator f = t.finite_faces_begin(); f != t.finite_faces_end();
-         ++f) {
-      int face_id = id_face(f);
+    for (auto f = t.finite_faces_begin(); f != t.finite_faces_end(); ++f) {
       for (int neighbor = 0; neighbor < 3; ++neighbor) {
         long distance =
             CGAL::squared_distance(f->vertex((neighbor + 1) % 3)->point(),
                                    f->vertex((neighbor + 2) % 3)->point());
-        int neighbor_id = id_face(f->neighbor(neighbor));
-        adj[face_id].push_back({neighbor_id, distance});
-        if (neighbor_id ==
-            INFINITE_FACE) { // also store neighbors of "infinite" face.
-          adj[INFINITE_FACE].push_back({face_id, distance});
+        int neighbor_id = f->neighbor(neighbor)->info();
+        adj[f->info()].push_back({neighbor_id, distance});
+
+        // also store neighbors of "infinite" face.
+        if (neighbor_id == INFINITE_FACE) {
+          adj[INFINITE_FACE].push_back({f->info(), distance});
         }
       }
     }
 
-    vector<long> dp(ids.size(), -1);
+    dp = vector<long>(curr_id, -1);
     priority_queue<pair<long, int>> pq;
     pq.push({LONG_MAX, INFINITE_FACE});
     while (pq.size()) {
@@ -81,25 +64,43 @@ int main() {
         pq.push({upd_distance, neighbor_id});
       }
     }
+  }
 
+  bool can_escape(Point p, long d) {
+    long dist = CGAL::squared_distance(p, t.nearest_vertex(p)->point());
+    return (dist >= d && dp[t.locate(p)->info()] >= 4 * d);
+  }
+
+private:
+  Delaunay t;
+  vector<long> dp;
+};
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  while (true) {
+    int n;
+    cin >> n;
+    if (n == 0)
+      break;
+    vector<Point> points;
+    points.reserve(n);
+    for (int i = 0; i < n; ++i) {
+      long x, y;
+      cin >> x >> y;
+      points.emplace_back(x, y);
+    }
+    Area area(n, points);
+    int m;
     cin >> m;
     for (int i = 0; i < m; ++i) {
+      long x, y, d;
       cin >> x >> y >> d;
-      Point point(x, y);
-      long default_distance =
-          CGAL::squared_distance(point, t.nearest_vertex(point)->point());
-      if (default_distance < d) {
-        cout << 'n';
-        continue;
-      }
-      auto face = t.locate(point);
-      int face_id = id_face(face);
-      long required_distance = dp[face_id];
-      if (required_distance < 4 * d) { // (2 * d)^2 = 4d^2
-        cout << 'n';
-      } else {
+      if (area.can_escape(Point(x, y), d))
         cout << 'y';
-      }
+      else
+        cout << 'n';
     }
     cout << '\n';
   }
--- src/week09/knights.cc
+++ src/week09/knights.cc
@@ -3,6 +3,8 @@
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/push_relabel_max_flow.hpp>
 
+using namespace std;
+
 using traits =
     boost::adjacency_list_traits<boost::vecS, boost::vecS, boost::directedS>;
 using graph = boost::adjacency_list<
@@ -11,11 +13,6 @@ using graph = boost::adjacency_list<
                     boost::property<boost::edge_residual_capacity_t, long,
                                     boost::property<boost::edge_reverse_t,
                                                     traits::edge_descriptor>>>>;
-
-using vertex_desc = traits::vertex_descriptor;
-using edge_desc = traits::edge_descriptor;
-
-using namespace std;
 
 class edge_adder {
   graph &G;
@@ -35,125 +32,81 @@ public:
   }
 };
 
-struct knight {
+struct Knight {
   int x, y;
 };
 
+int solve(int m, int n, int k, int c, vector<Knight> &knights) {
+  if (m == 0 || n == 0) {
+    return 0;
+  }
+
+  graph G(2 * m * n);
+  edge_adder edges(G);
+  const int v_source = boost::add_vertex(G);
+  const int v_sink = boost::add_vertex(G);
+  const auto v_id = [&](int y, int x, bool output) {
+    return y * m + x + (output ? n * m : 0);
+  };
+
+  // connect input and output
+  for (int y = 0; y < n; ++y) {
+    for (int x = 0; x < m; ++x) {
+      edges.add_edge(v_id(y, x, false), v_id(y, x, true), c);
+    }
+  }
+
+  // connect vertical segments
+  for (int y = 0; y < n - 1; ++y) {
+    for (int x = 0; x < m; ++x) {
+      edges.add_edge(v_id(y, x, true), v_id(y + 1, x, false), 1); // forward
+      edges.add_edge(v_id(y + 1, x, true), v_id(y, x, false),
+                     1); // backward
+    }
+  }
+
+  // connect horizontal segments
+  for (int y = 0; y < n; ++y) {
+    for (int x = 0; x < m - 1; ++x) {
+      edges.add_edge(v_id(y, x, true), v_id(y, x + 1, false), 1); // forward
+      edges.add_edge(v_id(y, x + 1, true), v_id(y, x, false),
+                     1); // backward
+    }
+  }
+
+  // connect output segments (horizontal)
+  for (int y = 0; y < n; ++y) {
+    edges.add_edge(v_id(y, 0, true), v_sink, 1);
+    edges.add_edge(v_id(y, m - 1, true), v_sink, 1);
+  }
+
+  // connect output segments (vertical)
+  for (int x = 0; x < m; ++x) {
+    edges.add_edge(v_id(0, x, true), v_sink, 1);
+    edges.add_edge(v_id(n - 1, x, true), v_sink, 1);
+  }
+
+  // connect the knights
+  for (const auto &knight : knights) {
+    edges.add_edge(v_source, v_id(knight.y, knight.x, false), 1);
+  }
+
+  return boost::push_relabel_max_flow(G, v_source, v_sink);
+}
+
 int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
   int t;
   cin >> t;
   while (t--) {
     int m, n, k, c;
     cin >> m >> n >> k >> c;
-    vector<knight> knights;
+    vector<Knight> knights;
     knights.reserve(k);
     for (int i = 0; i < k; ++i) {
       int x, y;
       cin >> x >> y;
       knights.push_back({x, y});
     }
-
-    // * 2 (input and output of intersections)
-    const int num_intersections = m * n;
-    const auto id_ix = [&](int x, int y, bool input) {
-      int id = y * m + x;
-      return (input ? 0 : num_intersections) + id;
-    };
-
-    constexpr bool HORIZONTAL = true, VERTICAL = false;
-    constexpr bool INPUT = true, OUTPUT = false;
-
-    const int num_vertical_segments = m * (n + 1);
-    const int num_horizontal_segments = n * (m + 1);
-    const int num_segments = num_vertical_segments + num_horizontal_segments;
-    const auto id_seg_raw = [&](int x, int y, bool direction) {
-      if (direction == VERTICAL) {
-        int id = x * (n + 1) + y;
-        return 2 * num_intersections + id;
-      } else {
-        int id = y * (m + 1) + x;
-        return 2 * num_intersections + num_vertical_segments + id;
-      }
-    };
-    const auto id_seg = [&](int x, int y, bool horizontal, bool input) {
-      int id = id_seg_raw(x, y, horizontal);
-      return (input ? 0 : num_segments) + id;
-    };
-    graph G(2 * num_intersections + 2 * num_segments);
-    edge_adder edges(G);
-
-    // connect input and output intersections
-    for (int x = 0; x < m; ++x) {
-      for (int y = 0; y < n; ++y) {
-        edges.add_edge(id_ix(x, y, INPUT), id_ix(x, y, OUTPUT), c);
-      }
-    }
-
-    // connect intersections with the segments
-    for (int x = 0; x < m; ++x) {
-      for (int y = 0; y < n; ++y) {
-        const int intersection_id = id_ix(x, y, OUTPUT);
-        edges.add_edge(intersection_id, id_seg(x, y, VERTICAL, INPUT), 1);
-        edges.add_edge(intersection_id, id_seg(x, y + 1, VERTICAL, INPUT), 1);
-
-        edges.add_edge(intersection_id, id_seg(x, y, HORIZONTAL, INPUT), 1);
-        edges.add_edge(intersection_id, id_seg(x + 1, y, HORIZONTAL, INPUT), 1);
-      }
-    }
-
-    // Add special vertices source and sink
-    const vertex_desc v_source = boost::add_vertex(G);
-    const vertex_desc v_sink = boost::add_vertex(G);
-
-    for (int x = 0; x < m; ++x) {
-      for (int y = 0; y < n + 1; ++y) {
-        edges.add_edge(id_seg(x, y, VERTICAL, INPUT),
-                       id_seg(x, y, VERTICAL, OUTPUT), 1);
-
-        if (y != 0) {
-          edges.add_edge(id_seg(x, y, VERTICAL, OUTPUT), id_ix(x, y - 1, INPUT),
-                         1);
-        } else {
-          edges.add_edge(id_seg(x, y, VERTICAL, OUTPUT), v_sink, 1);
-        }
-
-        if (y != n) {
-          edges.add_edge(id_seg(x, y, VERTICAL, OUTPUT), id_ix(x, y, INPUT), 1);
-        } else {
-          edges.add_edge(id_seg(x, y, VERTICAL, OUTPUT), v_sink, 1);
-        }
-      }
-    }
-
-    for (int x = 0; x < m + 1; ++x) {
-      for (int y = 0; y < n; ++y) {
-        edges.add_edge(id_seg(x, y, HORIZONTAL, INPUT),
-                       id_seg(x, y, HORIZONTAL, OUTPUT), 1);
-
-        if (x != 0) {
-          edges.add_edge(id_seg(x, y, HORIZONTAL, OUTPUT),
-                         id_ix(x - 1, y, INPUT), 1);
-        } else {
-          edges.add_edge(id_seg(x, y, HORIZONTAL, OUTPUT), v_sink, 1);
-        }
-
-        if (x != m) {
-          edges.add_edge(id_seg(x, y, HORIZONTAL, OUTPUT), id_ix(x, y, INPUT),
-                         1);
-        } else {
-          edges.add_edge(id_seg(x, y, HORIZONTAL, OUTPUT), v_sink, 1);
-        }
-      }
-    }
-
-    // Add the Knights to the Board
-    for (auto &kp : knights) {
-      edges.add_edge(v_source, id_ix(kp.x, kp.y, INPUT), 1);
-    }
-
-    long flow = boost::push_relabel_max_flow(G, v_source, v_sink);
-    std::cout << flow << '\n';
+    cout << solve(m, n, k, c, knights) << '\n';
   }
 }
